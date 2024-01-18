f795dab35a41a15015aaeb1596afc062a788c619
Mishaj__k <amstig100@gmail.com>
2024-01-18 12:30:26 +0530

    task 2

--- task2.js
+++ task2.js
@@ -1,15 +1,15 @@
 // if give a string to a function it should return that string in reverse order
 
 
-a = 'ladha';
+a = 'mishaj';
 length = a.length;
 //console.log(length);
 const revers = (a) => {
-    let jj= '';
+    let reversed= '';
  for(let i=length-1; i>=0;i--){
-     jj = jj+a[i]
+    reversed = reversed+a[i]
     if(i==0){
-        return jj;
+        return reversed;
     }
 }
 }
