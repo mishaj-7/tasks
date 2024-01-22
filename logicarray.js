f3c0e28349eba2264a14b5e9c5f7ccf429e1c975
Mishaj__k <amstig100@gmail.com>
2024-01-22 11:32:20 +0530

    logic array

---
+++ logicarray.js
@@ -0,0 +1,49 @@
+class MyArray {
+    constructor() {
+        this.data = {}
+        this.length = 0;
+    }
+
+    push(data) {
+        this.data[this.length] = data;
+        this.length++;
+    }
+
+    pop() {
+        
+        delete this.data[this.length - 1]
+        this.length--;
+    }
+
+    delete(index) {
+        delete this.data[index];
+        this.dataShift(index);
+    }
+
+    dataShift(index) {
+        for (let i = index; i < this.length; i++){
+            this.data[i] = this.data[i + 1];
+            delete this.data[this.length - 1];
+            this.length--;
+        }
+    }
+
+    addElement(index, element) {
+        
+        this.data[index] = element;
+    }
+
+
+}
+
+const newArray = new MyArray;
+
+newArray.push('mentrow');
+newArray.push('sujith');
+newArray.push('hdd');
+newArray.push('helo');
+newArray.addElement(4,'heeeee')
+
+
+
+console.log(newArray.data);
---
+++ task3.js
@@ -0,0 +1,32 @@
+// ar1 = [1,3,5,77,43,22]
+
+// ar2 = [32,64,86,22,212,5]
+
+// compare these two array any element is equals to each other in both array reutrn true else false pass two array to functon it
+// return true or false
+
+let ar1 = [1, 3, 77, 43,5];
+let ar2 = [32, 64, 86, 22, 212, 5];
+const findPair = (ar1, ar2) => {
+    let lenth = ar1.length;
+    let lenth2 = ar2.length;
+    
+    // console.log(lenth);
+    
+    if (lenth > lenth2) {
+        for (i = 0; i < lenth; i++) {
+            let j=0
+            if (ar1[i] === ar2[j]) {
+                
+            }
+            
+        }  
+    }
+
+    else {
+
+    }
+    
+};
+
+console.log(findPair(ar1,ar2));
